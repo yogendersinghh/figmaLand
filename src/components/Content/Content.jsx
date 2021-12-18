f0e778f785ed869e03368bf49a09e86d5a9df0f5
Yogender Singh <yogender.singh4343@gmail.com>
2021-12-18 22:52:42 +0530

    build

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import "./content.css";
+import Button from "../Button/Button";
+import image from "../../Assests/cardImage.png";
 const Content = () => {
   return (
     <div className="figmaland__content">
-      <div>
-        <div></div>
-        <div></div>
+      <div className="figmaland__content-top">
+        <div className="figmaland__content-top__heading">Contents</div>
+        <div className="figmaland__content-top__content">
+          We focus on ergonomics and meeting you where you work. It's only a
+          keystroke away.
+        </div>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="figmaland__content-bottom">
+        <div className="figmaland__content-bottom__card">
+          <div className="figmaland__content-bottom__card-heading">Work</div>
+          <div className="figmaland__content-bottom__card-content">
+            Ever wondered if you're too reliant on a client for work? Slate
+            helps you identify .
+          </div>
+          <div className="figmaland__content-bottom__card-button">
+            <Button content="Sign Up" backgroundColor="blue" />
+          </div>
+          <div className="figmaland__content-bottom__card-image">
+            <img src={image} alt="" />
+          </div>
+        </div>
+        <div className="figmaland__content-bottom__card">
+          <div className="figmaland__content-bottom__card-heading">Work</div>
+          <div className="figmaland__content-bottom__card-content">
+            Ever wondered if you're too reliant on a client for work? Slate
+            helps you identify .
+          </div>
+          <div className="figmaland__content-bottom__card-button">
+            <Button content="Sign Up" backgroundColor="blue" />
+          </div>
+          <div className="figmaland__content-bottom__card-image">
+            <img src={image} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
